fa9ffd1d0c0a6d9742b6e6079eb2e55f9b41f006
Jakub <truefear@gmail.com>
2015-09-01 15:08:40 +0200

    Write middleware.

--- Middleware/ResponseTimeMiddleware.php
+++ Middleware/ResponseTimeMiddleware.php
@@ -1,9 +1,16 @@
 <?php namespace jtauri\ResponseTimer\Middleware;
 
 use Closure;
+use jtauri\ResponseTimer\Timer;
 
 class ResponseTimeMiddleWare{
 	public function handle($request, Closure $next)
 	{
+
+		$timeStart = Timer::getMilliseconds();
+		$response = $next($request);
+		$timeEnd = Timer::getMilliseconds();
+		$response->headers->set("X-Response-Time", $timeEnd-$timeStart);
+		return $response;
 	}
 }
